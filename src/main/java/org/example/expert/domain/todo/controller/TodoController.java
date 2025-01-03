02d5512d123c4c9894998469754e4a7302b992bb
yeongbinim <yeim.dev@gmail.com>
2025-01-03 22:16:19 +0900

    refactor(todo): Controller에서 DTO로 변환하도록 변경

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
+import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -13,33 +14,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/todos")
 public class TodoController {
 
 	private final TodoService todoService;
 
-	@PostMapping("/todos")
+	@PostMapping
 	public ResponseEntity<TodoResponse> saveTodo(
 		@Auth AuthUser authUser,
 		@Valid @RequestBody TodoSaveRequest todoSaveRequest
 	) {
-		return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
+		Todo todo = todoService.createTodo(authUser, todoSaveRequest);
+
+		return ResponseEntity
+			.ok(TodoResponse.toDto(todo));
 	}
 
-	@GetMapping("/todos")
+	@GetMapping
 	public ResponseEntity<Page<TodoResponse>> getTodos(
 		@RequestParam(defaultValue = "1") int page,
 		@RequestParam(defaultValue = "10") int size
 	) {
-		return ResponseEntity.ok(todoService.getTodos(page, size));
+		Page<Todo> todos = todoService.getTodos(page, size);
+
+		return ResponseEntity
+			.ok(todos.map(TodoResponse::toDto));
 	}
 
-	@GetMapping("/todos/{todoId}")
+	@GetMapping("/{todoId}")
 	public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
-		return ResponseEntity.ok(todoService.getTodo(todoId));
+		Todo todo = todoService.getTodo(todoId);
+
+		return ResponseEntity
+			.ok(TodoResponse.toDto(todo));
 	}
 }
