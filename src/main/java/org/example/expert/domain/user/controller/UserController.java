002cead6476ef25e3284b7fa1df8b7d72268aaf7
yeongbinim <yeim.dev@gmail.com>
2025-01-03 21:55:32 +0900

    refactor(user): UserPasswordChangeRequest로 변경

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -3,25 +3,30 @@ package org.example.expert.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
-import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
+import org.example.expert.domain.user.dto.request.UserPasswordChangeRequest;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @GetMapping("/users/{userId}")
-    public ResponseEntity<UserResponse> getUser(@PathVariable long userId) {
-        return ResponseEntity.ok(userService.getUser(userId));
-    }
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<UserResponse> getUser(@PathVariable long userId) {
+		return ResponseEntity.ok(userService.getUser(userId));
+	}
 
-    @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
-        userService.changePassword(authUser.getId(), userChangePasswordRequest);
-    }
+	@PutMapping("/users")
+	public void changePassword(@Auth AuthUser authUser,
+		@RequestBody UserPasswordChangeRequest userPasswordChangeRequest) {
+		userService.changePassword(authUser.getId(), userPasswordChangeRequest);
+	}
 }
