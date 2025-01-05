6ed319d786ea3657dd319491e3fc49ad81e9e950
yeongbinim <yeim.dev@gmail.com>
2025-01-05 22:09:39 +0900

    refactor(entity): 각 컬럼에 이름 일관성 있게 지정, 양방향 참조 제거

--- src/main/java/org/example/expert/domain/manager/entity/Manager.java
+++ src/main/java/org/example/expert/domain/manager/entity/Manager.java
@@ -1,6 +1,13 @@
 package org.example.expert.domain.manager.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.expert.domain.todo.entity.Todo;
@@ -12,18 +19,20 @@ import org.example.expert.domain.user.entity.User;
 @Table(name = "managers")
 public class Manager {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false) // 일정 만든 사람 id
-    private User user;
-    @ManyToOne(fetch = FetchType.LAZY) // 일정 id
-    @JoinColumn(name = "todo_id", nullable = false)
-    private Todo todo;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false) // 일정 만든 사람 id
+	private User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY) // 일정 id
+	@JoinColumn(name = "todo_id", nullable = false)
+	private Todo todo;
 
-    public Manager(User user, Todo todo) {
-        this.user = user;
-        this.todo = todo;
-    }
+	public Manager(User user, Todo todo) {
+		this.user = user;
+		this.todo = todo;
+	}
 }
--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -1,15 +1,18 @@
 package org.example.expert.domain.todo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.common.entity.Timestamped;
-import org.example.expert.domain.manager.entity.Manager;
 import org.example.expert.domain.user.entity.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
@@ -17,32 +20,27 @@ import java.util.List;
 @Table(name = "todos")
 public class Todo extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String contents;
-    private String weather;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@Column(name = "title")
+	private String title;
 
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
-    private List<Comment> comments = new ArrayList<>();
+	@Column(name = "contents")
+	private String contents;
 
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
-    private List<Manager> managers = new ArrayList<>();
+	@Column(name = "weather")
+	private String weather;
 
-    public Todo(String title, String contents, String weather, User user) {
-        this.title = title;
-        this.contents = contents;
-        this.weather = weather;
-        this.user = user;
-        this.managers.add(new Manager(user, this));
-    }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    public void update(String title, String contents) {
-        this.title = title;
-        this.contents = contents;
-    }
+	public Todo(String title, String contents, String weather, User user) {
+		this.title = title;
+		this.contents = contents;
+		this.weather = weather;
+		this.user = user;
+	}
 }
