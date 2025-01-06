7abbb50bb6f7665fba5e321b42f0582693397c26
yeongbinim <yeim.dev@gmail.com>
2025-01-06 11:00:15 +0900

    lv3-2: 불필요한 if else 피하기

--- src/main/java/org/example/expert/domain/common/client/WeatherClient.java
+++ src/main/java/org/example/expert/domain/common/client/WeatherClient.java
@@ -3,6 +3,8 @@ package org.example.expert.domain.common.client;
 import java.net.URI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Optional;
 import org.example.expert.domain.common.client.dto.WeatherDto;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
@@ -23,26 +25,22 @@ public class WeatherClient {
 	public String getTodayWeather() {
 		ResponseEntity<WeatherDto[]> responseEntity =
 			restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
-
-		WeatherDto[] weatherArray = responseEntity.getBody();
 		if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
 			throw new RuntimeException(
 				"날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
-		} else {
-			if (weatherArray == null || weatherArray.length == 0) {
-				throw new RuntimeException("날씨 데이터가 없습니다.");
-			}
 		}
+
+		Optional<WeatherDto[]> optionalWeather = Optional.ofNullable(responseEntity.getBody());
+		WeatherDto[] weatherArray = optionalWeather.orElseThrow(
+			() -> new RuntimeException("날씨 데이터가 없습니다."));
 
 		String today = getCurrentDate();
 
-		for (WeatherDto weatherDto : weatherArray) {
-			if (today.equals(weatherDto.getDate())) {
-				return weatherDto.getWeather();
-			}
-		}
-
-		throw new RuntimeException("오늘에 해당하는 날씨 데이터를 찾을 수 없습니다.");
+		return Arrays.stream(weatherArray)
+			.filter(weather -> today.equals(weather.getDate()))
+			.findFirst()
+			.map(WeatherDto::getWeather)
+			.orElseThrow(() -> new RuntimeException("오늘에 해당하는 날씨 데이터를 찾을 수 없습니다."));
 	}
 
 	private URI buildWeatherApiUri() {
